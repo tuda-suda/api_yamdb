db1486c4667fcae934f68dee6e9d2dd1db831329
bzhekshiev <bzhekshiev@gmail.com>
2020-08-25 17:54:08 +0300

    bugfix

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -121,7 +121,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
--- yamdb/views.py
+++ yamdb/views.py
@@ -3,7 +3,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics, mixins, permissions, viewsets
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
+
 from users.permissions import IsAdmin, IsModerator, IsOwner, ReadOnly
 
 from .models import Category, Genre, Title
@@ -18,9 +19,9 @@ class CategoryViewSet(mixins.CreateModelMixin,
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes_by_action = {
-        'create': [IsAdmin | ReadOnly],
-        'list': [ReadOnly],
-        'destroy': [IsAdmin | ReadOnly]}
+        'create': (IsAdmin,),
+        'list': (AllowAny,),
+        'destroy': (IsAdmin,)}
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = ('slug')
@@ -33,9 +34,9 @@ class GenreViewSet(mixins.CreateModelMixin,
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes_by_action = {
-        'create': [IsAdmin],
-        'list': [ReadOnly],
-        'destroy': [IsAdmin]}
+        'create': (IsAdmin,),
+        'list': (AllowAny,),
+        'destroy': (IsAdmin,)}
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = ('slug')
@@ -44,13 +45,11 @@ class GenreViewSet(mixins.CreateModelMixin,
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    permission_classes_by_action = {
-        'create': [IsAdmin],
-        'list': [ReadOnly],
-        'destroy': [IsAdmin],
-        'update': [IsAdmin]}
+    permission_classes = (AllowAny,)
+    # permission_classes_by_action = {
+    #     'create': (IsAdmin,),
+    #     'list': (AllowAny,),
+    #     'destroy': (IsAdmin,),
+    #     'update': (IsAdmin,)}
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'year', 'category', 'genre')
-
-
-    
