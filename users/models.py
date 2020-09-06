6f6734ef1522d8aca8bef362bbdf28353544b53a
Marat Gumerov <marat.gumerov99@gmail.com>
2020-09-06 11:55:37 +0500

    Set default username for users

--- users/models.py
+++ users/models.py
@@ -11,8 +11,10 @@ class UserRoles(models.TextChoices):
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
         user = self.model(
-            email=self.normalize_email(email),
+            email=email,
+            username=f'user{str(hash(email))[:8]}',
             **extra_fields
         )
         user.set_password(password)
